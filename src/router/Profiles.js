b43bb670a5ccdd1ef71ac0e7ea4582f5234e8d81
sungyun <sungyun5423@gmail.com>
2021-03-03 23:34:47 +0900

    feat/react-router(NavLink, withRouter, Switch, history)

--- src/router/Profiles.js
+++ src/router/Profiles.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
   return (
@@ -8,15 +9,20 @@ const Profiles = () => {
       <h3>유저 목록: </h3>
       <ul>
         <li>
-          <Link to="/profiles/sungyun">sungyun</Link>
+          <NavLink to="/profiles/sungyun" activeStyle={{ background: 'black', color: 'white' }}>
+            sungyun
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/gildong">gildong</Link>
+          <NavLink to="/profiles/gildong" activeStyle={{ background: 'black', color: 'white' }}>
+            gildong
+          </NavLink>
         </li>
       </ul>
 
       <Route path="/profiles" exact render={() => <div>유저를 선택하세요</div>} />
       <Route path="/profiles/:username" component={Profile} />
+      <WithRouterSample />
     </div>
   );
 };
