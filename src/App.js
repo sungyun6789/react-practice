beb1099ac4a5db67d8a26604bfd3d14a386dcd9a
sungyun <sungyun5423@gmail.com>
2021-03-11 12:34:35 +0900

    feat: redux module(counter, todo)

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
-import About from './router/About';
-import HistorySample from './router/HistorySample';
-import Home from './router/Home';
-import Profiles from './router/Profiles';
 
 const App = () => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/profiles">사용자</Link>
-        </li>
-        <li>
-          <Link to="/history">예제</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
-        <Route
-          // path를 따로 설정하지 않으면 모든 상황에 렌더링됨
-          render={({ location }) => (
-            <div>
-              <h2>이 페이지는 존재하지 않습니다</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
-        />
-      </Switch>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
