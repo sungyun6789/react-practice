91c0c0978429b0dc89b53d46bd639a5045381f7c
sungyun <sungyun5423@gmail.com>
2021-03-15 22:32:00 +0900

    redux-sage + generator, 비동기 카운터

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import PostListPage from './middleware/pages/PostListPage';
 import PostPage from './middleware/pages/PostPage';
+import CounterContainer from './middleware/containers/CounterContainer'
 
 const App = () => {
   return (
     <>
+      <CounterContainer />
       <Route path="/" component={PostListPage} exact={true} />
       <Route path="/:id" component={PostPage} />
     </>
--- src/middleware/modules/index.js
+++ src/middleware/modules/index.js
@@ -1,7 +1,11 @@
+import { all } from '@redux-saga/core/effects';
 import { combineReducers } from 'redux';
-import counter from './counter';
+import counter, { counterSaga } from './counter';
 import posts from './posts';
 
 const rootReducer = combineReducers({ counter, posts });
+export function* rootSaga() {
+  yield all([counterSaga()]);
+}
 
 export default rootReducer;
