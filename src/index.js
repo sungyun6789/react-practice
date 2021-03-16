4ecb3da8d695faf9194eea6e2225b0f3ae4bfd6f
sungyun <sungyun5423@gmail.com>
2021-03-16 22:25:57 +0900

    feat: redux-saga + react-router

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ import { createBrowserHistory } from 'history';
 import createSagaMiddleware from 'redux-saga';
 
 const customHistory = createBrowserHistory();
-const sagaMiddleware = createSagaMiddleware();  // 사가 미들웨어를 만듭니다
+const sagaMiddleware = createSagaMiddleware({
+  context: {
+    history: customHistory,
+  },
+}); // 사가 미들웨어를 만듭니다
 
 const store = createStore(
   rootReducer,
@@ -24,7 +28,7 @@ const store = createStore(
       logger,
     ),
   ),
-);  // 여러개의 미들웨어를 적용 할 수 있습니다
+); // 여러개의 미들웨어를 적용 할 수 있습니다
 
 sagaMiddleware.run(rootSaga); // 루트 사가를 실행합니다
 // 주의: 스토어 생성이 된 다음에 위 코드를 실행
