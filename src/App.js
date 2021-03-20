13b67b9b1b19282777a3346bba5b54e23dcc8bb3
sungyun <sungyun5423@gmail.com>
2021-03-20 15:08:40 +0900

    feat: add category

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import Categories from './news-viewer/components/Categories';
 import NewsList from './news-viewer/NewsList';
 
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
 };
 
 export default App;
--- src/news-viewer/NewsList.js
+++ src/news-viewer/NewsList.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=cb8b3b41436842adb90436a62c987503',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=cb8b3b41436842adb90436a62c987503`,
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -34,7 +35,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   // 대기 중
   if (loading) {
