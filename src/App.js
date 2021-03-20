a9e5676d0cb5a5ae555bc98436847e8157fd4a16
sungyun <sungyun5423@gmail.com>
2021-03-20 14:43:07 +0900

    feat: get News List

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import PostListPage from './middleware/pages/PostListPage';
-import PostPage from './middleware/pages/PostPage';
-import CounterContainer from './middleware/containers/CounterContainer';
+import React, { useState } from 'react';
+import NewsList from './news-viewer/NewsList';
 
 const App = () => {
-  return (
-    <>
-      <CounterContainer />
-      <Route path="/" component={PostListPage} exact={true} />
-      <Route path="/:id" component={PostPage} />
-    </>
-  );
+  return <NewsList />;
 };
 
 export default App;
---
+++ src/news-viewer/NewsList.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=cb8b3b41436842adb90436a62c987503',
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  // 대기 중
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
+  // articles 값이 유효할 때
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
