888c86b872bf0653ff21d55ac6ec36959c0b5d7d
sungyun <sungyun5423@gmail.com>
2021-04-16 21:54:10 +0900

    practical-react-programming project setting

--- src/index.js
+++ src/index.js
@@ -1,53 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import VirtualTable from './react-window/list';
+import App from './App';
 
-const columns = [
-  {
-    title: 'A',
-    dataIndex: 'key',
-    width: 150,
-  },
-  {
-    title: 'B',
-    dataIndex: 'key',
-  },
-  {
-    title: 'C',
-    dataIndex: 'key',
-  },
-  {
-    title: 'D',
-    dataIndex: 'key',
-  },
-  {
-    title: 'E',
-    dataIndex: 'key',
-    width: 200,
-  },
-  {
-    title: 'F',
-    dataIndex: 'key',
-    width: 100,
-  },
-];
-const data = Array.from(
-  {
-    length: 100000,
-  },
-  (_, key) => ({
-    key,
-  }),
-);
-
-ReactDOM.render(
-  <VirtualTable
-    columns={columns}
-    dataSource={data}
-    scroll={{
-      y: 300,
-      x: '100vw',
-    }}
-  />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
