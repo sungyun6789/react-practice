ddc3f14524693bf0f5257f710fe1df055ef69c40
sungyun <sungyun5423@gmail.com>
2021-04-19 21:45:52 +0900

    feat: useRef를 사용해 값 저장하기

--- src/practical-react/ref/Ref.js
+++ src/practical-react/ref/Ref.js
@@ -1,37 +1,28 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const Ref = () => {
-  const inputRef = useRef();
-  const buttonRef = useRef();
+  const [age, setAge] = useState(20);
+  const prevAgeRef = useRef(20);
   useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+    prevAgeRef.current = age;
+  }, [age]);
+
+  const prevAge = prevAgeRef.current;
+  const text = age === prevAge ? 'same' : age > prevAge ? 'older' : 'younger';
+
   return (
     <div>
-      <InputAndSave inputRef={inputRef} />
-      <Button ref={buttonRef} />
-      <button onClick={() => inputRef.current.focus()}>텍스트로 이동</button>
+      <p>{`age ${age} is ${text} than age ${prevAge}`}</p>
+      <button
+        onClick={() => {
+          const age = Math.floor(Math.random() * 50 + 1);
+          setAge(age);
+        }}
+      >
+        나이 변경
+      </button>
     </div>
   );
 };
 
 export default Ref;
-
-function InputAndSave({ inputRef, buttonRef }) {
-  return (
-    <div>
-      <input type="text" ref={inputRef} />
-      <button ref={buttonRef}>저장</button>
-    </div>
-  );
-}
-
-const Button = React.forwardRef(function ({ onClick }, ref) {
-  return (
-    <button onClick={onClick} ref={ref}>
-      저장
-    </button>
-  );
-});
-
-// ref를 조건부 렌더링에서 사용할 때는 current 속성을 검사하는 코드를 작성해야 한다.
