8f61565e936cc3577708744c2febc8bbb0e40c6d
sungyun <sungyun5423@gmail.com>
2021-04-24 23:07:52 +0900

    렌더링 속도를 올리기 위한 성능 최적화1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import EffectComponent from './practical-react/useEffect/EffectComponent';
+import { useState } from 'react';
+import MyComponent from './practical-react/render/MyComponent';
 
 export default function App() {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
   return (
     <div>
-      <EffectComponent />
+      <p>실전 리액트</p>
+      <button onClick={() => setValue1(value1 + 1)}>value1 증가</button>
+      <button onClick={() => setValue2(value2 + 1)}>value2 증가</button>
+      <MyComponent value1={value1} value2={value2} />
     </div>
   );
 }
